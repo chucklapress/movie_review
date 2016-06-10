329a2e00456ff2c94009518389f44888bfb8b384
chucklapress <charleslapress@gmail.com>
2016-06-10 11:08:17 -0400

    changes

--- moviereviewapp/admin.py
+++ moviereviewapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from moviereviewapp.models import Rater
+from moviereviewapp.models import Movie
+from moviereviewapp.models import Review
 
 # Register your models here.
 
 
 admin.site.register(Rater)
+admin.site.register(Movie)
+admin.site.register(Review)
