bb213872cb40eae7abeed01d57f8bb6b83ecee12
chucklapress <charleslapress@gmail.com>
2016-06-12 12:04:30 -0400

    changes to working weekend project

--- movie_review/urls.py
+++ movie_review/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from moviereviewapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index_view),
+    url(r'^movie$',views.movie_list),
+
+
+
 ]
--- moviereviewapp/views.py
+++ moviereviewapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from moviereviewapp.models import Rater, Movie, Review
+
 
 # Create your views here.
+def index_view(request):
+    return render(request, "index.html")
+
+def movie_list(request):
+
+    context ={
+       "Movies": Movie.objects.all()
+    }
+    return render(request, "movie.html", context)
+
+
+
+
